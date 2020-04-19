f7e50b4686ca7b2a6c1ca0b7ad842c0cf5c11f18
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-19 14:57:08 -0500

    Event bubbling practice

---
+++ Event_handling/script.js
@@ -0,0 +1,36 @@
+var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
+
+function prepareRead() {
+  // Create the countdown timer.
+}
+
+function speedRead() {
+  // Print words to the screen one at a time.
+}
+
+
+
+function add(x)
+{
+    var inputarray=[1,2,3];
+    var count =0;
+    for(var i=0; i< inputarray.length;i++)
+    {
+count += inputarray[i];
+
+    }
+    console.log(count);
+return count;
+}
+function add(x)
+{
+    var inputarray=[1,2,3];
+    var count =0;
+    for(var i=0; i< inputarray.length;i++)
+    {
+count += inputarray[i];
+
+    }
+    console.log(count);
+ return add(count);
+}
